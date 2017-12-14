4a7f19e01457a41970d76bdebc0225a824cb79bc
Adel Oktariza <oktariza.adel@gmail.com>
2017-12-14 16:52:29 +0700

    add image parallax and jquery-3.2.1

--- application/views/includes/footer.php
+++ application/views/includes/footer.php
@@ -1,1 +1,9 @@
+
+<script type="text/javascript" src="<?php echo base_url();?>assets/js/jquery-3.2.1.js"></script>
+<script type="text/javascript" src="<?php echo base_url();?>assets/js/jquery-3.2.1.min.js"></script>
+<script type="text/javascript" src="<?php echo base_url();?>assets/js/bootstrap.js"></script>
+<script type="text/javascript" src="<?php echo base_url();?>assets/js/bootstrap.min.js"></script>
+<script type="text/javascript" src="<?php echo base_url();?>assets/js/parallax.js"></script>
+<script type="text/javascript" src="<?php echo base_url();?>assets/js/parallax.min.js"></script>
+
 </html>
--- application/views/view_home.php
+++ application/views/view_home.php
@@ -1,23 +1,15 @@
+
 <?php include 'includes/header.php'; ?>
+
 <body>
 
-<div id="container">
-	<h1>Welcome to CodeIgniter!</h1>
+	<div class="parallax-window" data-parallax="scroll" data-image-src="<?php echo base_url();?>assets/images/space.jpg"></div>
+	<div class="parallax-window" data-parallax="scroll" data-image-src="<?php echo base_url();?>assets/images/space.jpg"></div>
 
-	<div id="body">
-		<p>The page you are looking at is being generated dynamically by CodeIgniter.</p>
+	<div class="parallax-window" data-parallax="scroll" data-image-src="<?php echo base_url();?>assets/images/space.jpg"></div>
 
-		<p>If you would like to edit this page you'll find it located at:</p>
-		<code>application/views/welcome_message.php</code>
+	<div class="parallax-window" data-parallax="scroll" data-image-src="<?php echo base_url();?>assets/images/space.jpg"></div>
 
-		<p>The corresponding controller for this page is found at:</p>
-		<code>application/controllers/Welcome.php</code>
-
-		<p>If you are exploring CodeIgniter for the very first time, you should start by reading the <a href="user_guide/">User Guide</a>.</p>
-	</div>
-
-	<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds. <?php echo  (ENVIRONMENT === 'development') ?  'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '' ?></p>
-</div>
 
 </body>
 
