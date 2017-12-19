1ce5021beb63ef35ac06670c616fbf18da1ab3d6
Adel Oktariza <oktariza.adel@gmail.com>
2017-12-19 16:03:24 +0700

    add tampilan login

--- application/views/view_login_user.php
+++ application/views/view_login_user.php
@@ -3,18 +3,33 @@
 
 <body>
 
-	<div class="col-md-12 image-background">
-		<div class="col-md-4 form-login">
-			<?php echo form_open("login/cek_login"); ?>
-			<p>Username : <br>
-			<input type="text" name="username">
-			</p>
-			<p>Password : <br>
-			<input type="password" name="password"></p>
-			<p><button type="submit">Submit</button></p>
-			<?php echo form_close(); ?>
+	<div class="image-frame">
+		<div class="image-background">
 		</div>
-		
+
+		<?php echo form_open("login/cek_login"); ?>
+
+		<div class="card card-container">
+            <img id="profile-img" class="profile-img-card" src="//ssl.gstatic.com/accounts/ui/avatar_2x.png" />
+            <p id="profile-name" class="profile-name-card"></p>
+            <form class="form-signin">
+                <span id="reauth-email" class="reauth-email"></span>
+                <input type="text" id="inputUsername" name="username" class="form-control" placeholder="Username" required autofocus>
+                <input type="password" id="inputPassword" name="password" class="form-control" placeholder="Password" required>
+                <div id="remember" class="checkbox">
+                    <label>
+                        <input type="checkbox" value="remember-me"> Remember me
+                    </label>
+                </div>
+                <button class="btn btn-lg btn-primary btn-block btn-signin" type="submit">Sign in</button>
+            </form><!-- /form -->
+            <a href="#" class="forgot-password">
+                Forgot the password?
+            </a>
+        </div>
+
+        <?php echo form_close(); ?>
+
 	</div>
 </body>
 
